2fd7cd4df5bb08a9ff6bfa264a401ee042dacf80
alenlajeesh <alenlajeesh@gmail.com>
2026-03-06 12:50:47 +0530

    feat: implement basic TCP socket server

---
+++ src/server.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#include "../include/server.h"
+
+void start_server(int port){
+	int server_fd;
+	int client_fd;
+
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+
+	socklen_t client_len= sizeof(client_addr);
+
+	server_fd =socket(AF_INET,SOCK_STREAM,0);
+	if(server_fd<0){
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	server_addr.sin_family= AF_INET;
+	server_addr.sin_addr.s_addr =INADDR_ANY;
+	server_addr.sin_port=htons(port);
+
+	if(bind(server_fd,(struct sockaddr *)&server_addr,sizeof(server_addr))<0){
+		perror("bind failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+
+	if(listen(server_fd,10)<0){
+		perror("listen failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Server listening on port %d\n",port);
+
+	while(1){
+		client_fd=accept(server_fd,(struct sockaddr *)&client_addr,&client_len);
+
+		if(client_fd<0){
+			perror("accept failed");
+			continue;
+		}
+		printf("Client connected\n");
+		close(client_fd);
+	}
+}
