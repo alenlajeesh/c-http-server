b4e9cbd8cd6879441217ed9f233d0ac6ee187e98
alenlajeesh <alenlajeesh@gmail.com>
2026-03-06 16:34:54 +0530

    feat: add logging, content-type detection, and automatic index routing

--- include/response.h
+++ include/response.h
@@ -2,6 +2,7 @@
 #define RESPONSE_H
 
 void send_file_response(int client_fd, const char *path);
-void send_404(int client_fd);
+void send_404(int client_fd,const char *path);
+const char* get_content_type(const char *path);
 
 #endif
